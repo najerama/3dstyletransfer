9313312120da8843038608d7055a1c0877f3eadc
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-06 19:07:22 +0000

    to handle breaks while runnign

--- style_transfer_3d/examples/wrapper_runs_batch.py
+++ style_transfer_3d/examples/wrapper_runs_batch.py
@@ -1,6 +1,9 @@
 from configargparse import ArgParser
 from time import time
 from os.path import join as pjoin
+from os.path import exists
+from datetime import datetime
+
 #import subprocess
 from subprocess import Popen, PIPE
 import pandas as pd
@@ -12,14 +15,21 @@ def main(args):
     
     output = dict()
 
+    output_pth = pjoin(args.out_dir,"style_output.csv")
+    idx = []
+    if exists(output_pth):
+        idx = pd.read_csv(output_pth, index_col =0 ).index
+        ##append
+
     for cf in cfiles:
         cn = cf.split("/")[-1]
         for sf in sfiles:
-            
             sn = sf.split("/")[-1]
-
-            t = time()
             row = cn+"_"+sn
+            if row in idx:
+                print("Skipping %s as already exists" %(row))
+                continue    
+            t = time()
             #print(subprocess.call('python ./examples/run.py -im %s.obj -is %s.jpg -o %s.gif -lc %d -ltv %d' % (cf,sf,pjoin(args.out_dir,row),args.lc,args.ltv) ,shell=True))
             #content, style, tv = subprocess.STDOUT 
 
@@ -34,11 +44,16 @@ def main(args):
                             'style_loss':style,
                             'tv_loss':tv,
                             'overall_loss': loss,
-                            'time':  int(time()-t) }
+                            'time':  int(time()-t),
+                            'current_time': datetime.now()}
             
-    print(output)
-    #output
-    pd.DataFrame.from_dict(output,orient="index").to_csv(pjoin(args.out_dir,"style_output.csv"))
+        #print(output)
+            if exists(output_pth):
+                file_ = pd.read_csv(output_pth,index_col=0)
+                ##append
+                pd.concat([file_,pd.DataFrame.from_dict(output,orient="index")],axis = 0).to_csv(output_pth)
+            else:
+                pd.DataFrame.from_dict(output,orient="index").to_csv(output_pth)
 
 if __name__ == "__main__":
     p = ArgParser()
