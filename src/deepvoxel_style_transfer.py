44d291947bd61d0a83500bca0af53c4eab6f393c
Shubham Agrawal <agshubh191@gmail.com>
2019-11-25 16:08:54 -0500

    Add img-to-img style transfer
    - Working!
    - Some issue causes red dots to appear. Not major.

--- src/deepvoxel_style_transfer.py
+++ src/deepvoxel_style_transfer.py
@@ -3,41 +3,28 @@ from torchvision import models
 import numpy as np
 from PIL import Image
 from functools import reduce
+import argparse
+from tqdm import tqdm
+import os
+from tensorboardX import SummaryWriter
 from deepvoxels.util import custom_load 
 from deepvoxels import data_util
+import utils
+
 
 class StyleTransferModel:
-    def __init__(
-        self,
-        filepath_style,
-        img_size
-    ):
-        self.img_size = img_size
-        self.device = torch.device('cuda')
-
-        # Load style image
-        reference_image = self.load_rgb(filepath_style)
-        reference_image = reference_image[None, :, :, :]
-        reference_image = torch.tensor(reference_image, device=self.device, dtype=torch.float)
+    def __init__(self):
+        self.device = data_util.get_device()
 
         # load style transfer model
         vgg16 = models.vgg16(pretrained=True)
         self.style_model = list(vgg16.features.children())[:23]
         for l in self.style_model:
             l.to(self.device)
+            l.eval()
+            for parameters in l.parameters():
+                parameters.requires_grad = False    
         self.feature_layers = [3, 8, 15, 22]
-
-        # cache style features for style image
-        with torch.no_grad():
-            self.features_ref = self.extract_style_features(reference_image)            
-        self.features_ref = [feat.to(self.device) for feat in self.features_ref]
-
-    def load_rgb(self, path):
-        # Borrowed from class NovelViewTriplets
-        img = data_util.load_img(path, square_crop=True, downsampling_order=1, target_size=self.img_size)
-        img = img[:, :, :3].astype(np.float32) / 255. - 0.5
-        img = img.transpose(2,0,1)
-        return img
 
     def extract_style_features(self, images):
         out = images
@@ -53,25 +40,83 @@ class StyleTransferModel:
                 results.append(feat1)
         return results
 
-    def get_style_loss(self, features):
-        loss  = [torch.sum((f-fr)**2) for f, fr in zip(features, self.features_ref)]
+    def extract_content_features(self, images):
+        out = images
+        results = []
+        for i, l in enumerate(self.style_model):
+            out = l(out)
+            if i in self.feature_layers:
+                results.append(out)
+        return results
+
+    def get_loss(self, feat, feat_ref):
+        loss  = [torch.sum((f-fr)**2) for f, fr in zip(feat, feat_ref)]
         loss_overall = torch.tensor(0., device=self.device)
         for l in loss:
             loss_overall = loss_overall + l
-        batch_size = features[0].shape[0]
+        batch_size = feat[0].shape[0]
         loss_overall = loss_overall/batch_size
         return loss_overall
 
-if __name__ == "__main__":
-    # Extract style features for a dummy image
-    dummy_image_path = "../deepvoxels-data/synthetic_scene/test/bus_test/rgb/000001.png"
-    reference_image = Image.open(dummy_image_path)
-    reference_image = np.array(reference_image)[:, :, :3]
-    reference_image = reference_image.transpose((2,0,1))[None, :, :, :]
-    reference_image = torch.tensor(reference_image, device=torch.device("cuda"), dtype=torch.float)
+    def get_style_loss(self, feat_src, feat_dst):
+        return self.get_loss(feat_src, feat_dst)
 
-    style_image_path = "../style_transfer_3d/examples/data/styles/bailly1.jpg"
-    style_transfer_model = StyleTransferModel(style_image_path, (512, 512))
-    style_features = style_transfer_model.extract_style_features(reference_image)
-    style_loss = style_transfer_model.get_style_loss(style_features) 
-    print(style_loss)
+    def get_content_loss(self, feat_src, feat_dst):
+        return self.get_loss(feat_src, feat_dst)
+
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-src", "--src_img_path", required=True, type=str)
+    parser.add_argument("-style", "--style_img_path", required=True, type=str)
+    parser.add_argument("-n", "--num_iters", required=True, type=int)
+    parser.add_argument("-l", "--log_dir", required=True, type=str)
+    parser.add_argument("-sc", "--style_coeff", default=0.5, type=float)
+    parser.add_argument("-cc", "--content_coeff", default=0.5, type=float)
+
+    args = parser.parse_args()
+
+    run_dir = os.path.join(args.log_dir, "runs", "img_style_transfer", utils.get_log_dir())
+    if not os.path.exists(run_dir):
+        os.makedirs(run_dir)
+    writer = SummaryWriter(run_dir, flush_secs=10)
+    img_size = (512, 512)
+    device = data_util.get_device()
+
+    # Load Images
+    src_img = utils.load_rgb(args.src_img_path, img_size)
+    style_img = utils.load_rgb(args.style_img_path, img_size)
+    writer.add_image("Input images src style", np.concatenate((src_img, style_img), axis=-1)+0.5, 0)
+
+    # Prepare Images
+    src_img = np.expand_dims(src_img, axis=0)
+    src_img = torch.tensor(src_img, device=device, dtype=torch.float)
+    style_img = np.expand_dims(style_img, axis=0)
+    style_img = torch.tensor(style_img, device=device, dtype=torch.float)
+    
+    stm = StyleTransferModel()
+    style_features = stm.extract_style_features(style_img)
+    content_features = stm.extract_content_features(src_img)
+
+    src_img.requires_grad = True
+    optimizer = torch.optim.Adam([src_img])
+    
+    for num_iter in tqdm(range(args.num_iters)):
+        optimizer.zero_grad()
+        sf = stm.extract_style_features(src_img)
+        sl = stm.get_style_loss(sf, style_features)
+
+        cf = stm.extract_content_features(src_img)
+        cl = stm.get_content_loss(cf, content_features)
+
+        l = (args.style_coeff) * sl + (args.content_coeff) * cl
+        l.backward()
+        optimizer.step()
+        if num_iter%10:
+            writer.add_scalars("Loss", {
+                "style loss(scaled)": sl.item() * args.style_coeff, 
+                "content loss(scaled)": cl.item() * args.content_coeff,
+                "ovearll loss": l.item()
+            }, num_iter)
+            writer.add_image("Style Transfer Image", src_img+0.5, num_iter)
